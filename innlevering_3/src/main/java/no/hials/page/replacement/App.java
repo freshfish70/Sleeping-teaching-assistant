48e1604459bb4714fbe1a099a76eb0d54c1d8c45
Christoffer A Træen <christoffer.andersen@live.com>
2020-03-07 12:30:16 +0100

    WIP fundamental ready

--- innlevering_3/src/main/java/no/hials/page/replacement/App.java
+++ innlevering_3/src/main/java/no/hials/page/replacement/App.java
@@ -2,9 +2,11 @@ package no.hials.page.replacement;
 
 public class App {
 
+    private static final int FRAMES = 3;
 
     public static void main(String[] args){
         OptimalReplacement or = new OptimalReplacement();
+        or.setup(FRAMES);
         or.process("7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1");
     }
 }
--- innlevering_3/src/main/java/no/hials/page/replacement/OptimalReplacement.java
+++ innlevering_3/src/main/java/no/hials/page/replacement/OptimalReplacement.java
@@ -1,5 +1,6 @@
 package no.hials.page.replacement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,29 +8,66 @@ import java.util.List;
  * Fill in your code in this class!
  */
 public class OptimalReplacement extends ReplacementAlgorithm {
-    
+
     // TODO - add some state variables here, if you need any
 
     @Override
     protected void reset() {
+        System.out.println("reset");
         // TODO - do preparation/initilization here, if needed
     }
-    
+
     @Override
     public int process(String referenceString) {
         List<Integer> pageReferences = Tools.stringToArray(referenceString);
         if (pageReferences == null) return 0;
-        
+
         int replacements = 0; // How many page replacements made
-        
-        // TODO - process the reference string here. You can see FIFOReplacement
-        // as an example. But remember, that FIFO uses a different algorithm.
-        // This class should use Optimal Replacement algorithm, described
-        // in Section 9.4.
-        // Get the reference list as an array
-       
+        int maxFrames = this.frames.length;
+        for (int i = 0; i < pageReferences.size(); i++) {
+            int newPage = pageReferences.get(i);
+            int frameIndex = 0;
+            boolean pageInserted = false;
+            while (frameIndex < maxFrames && this.frames[frameIndex] != newPage && !pageInserted) {
+                if (this.frames[frameIndex] == EMPTY) {
+                    this.frames[frameIndex] = newPage;
+                    pageInserted = true;
+                } else {
+                    int longestIndex = EMPTY;
+                    int replaceWith = EMPTY;
+
+                    for (int j = 0; j < this.frames.length; j++) {
+                        int currentFrameToFind = this.frames[j];
+                        for (int k = i + 1; k < pageReferences.size(); k++) {
+                            if (pageReferences.get(k) == currentFrameToFind) {
+                                if (longestIndex < j) {
+                                    longestIndex = j;
+                                    replaceWith = currentFrameToFind;
+                                }
+                            }
+                        }
+                    }
+
+                    if (longestIndex != EMPTY && replaceWith != EMPTY){
+                        this.frames[frameIndex] = replaceWith;
+                        pageInserted = true;
+                    }
+                }
+                frameIndex++;
+            }
+            StringBuilder b = new StringBuilder();
+            for (int j = 0; j < frames.length; j++) {
+                b.append("FRAME: ");
+                b.append(frames[j]);
+                b.append(" : ");
+            }
+            System.out.println(b.toString());
+        }
+
         return replacements;
     }
 
+
     // TODO - create any helper methods here if you need any
+
 }
