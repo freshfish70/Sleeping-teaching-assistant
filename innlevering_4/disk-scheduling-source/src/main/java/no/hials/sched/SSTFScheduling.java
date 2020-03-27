2d2d0fb18741acb8216e5ab554d21b40c60f1d6b
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-03-27 14:22:03 +0100

    done with algo on inlv 4

--- innlevering_4/disk-scheduling-source/src/main/java/no/hials/sched/SSTFScheduling.java
+++ innlevering_4/disk-scheduling-source/src/main/java/no/hials/sched/SSTFScheduling.java
@@ -18,13 +18,51 @@ public class SSTFScheduling extends DiskSchedulingAlgo {
         }
         
         int movement = 0; // Total head movement
+        int headPos = this.getHeadPosition();
         
         // TODO - fill in your algorithm here
         // Detect which position to move to in each step and call
         // moveHead()
         // See FCFSScheduling as an example
-       
+
+        List<Integer> posList = refList;
+        int s = refList.size();
+
+        for (int i = 0; i < s; i++) {
+            int nextPosIndex = this.getNextPos(this.getHeadPosition(), posList);
+            int nextPos = posList.get(nextPosIndex);
+            int mv = this.moveHead(nextPos);
+
+            if (mv < 0) {
+                // Error
+                return -1;
+            }
+
+            movement += mv;
+            posList.remove(nextPosIndex);
+
+        }
+
         return movement;
+    }
+
+
+    private int getDelta(int oldPos, int newPos){
+        return Math.abs(oldPos - newPos);
+    }
+
+    private int getNextPos(int currPos,List<Integer> remainingRefs){
+        int candIndex = 0;
+        int candDelta = this.getDelta(currPos, remainingRefs.get(0));
+
+        for (int i = 0; i < remainingRefs.size(); i++) {
+            int d = this.getDelta(currPos, remainingRefs.get(i));
+            if (d < candDelta) {
+                candIndex = i;
+                candDelta = d;
+            }
+        }
+        return candIndex;
     }
 
 }
